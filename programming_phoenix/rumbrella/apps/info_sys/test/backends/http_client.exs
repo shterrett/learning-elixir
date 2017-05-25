3d80d8d720a327f98d40c28b84305c1126c007c4
Stuart Terrett <shterrett@gmail.com>
2017-05-25 13:57:19 -0400

    Programming Phoenix Ch 13

---
+++ programming_phoenix/rumbrella/apps/info_sys/test/backends/http_client.exs
@@ -0,0 +1,11 @@
+defmodule InfoSys.Test.HTTPClient do
+  @wolfram_xml File.read!("test/fixtures/wolfram.xml")
+
+  def request(url) do
+    url = to_string(url)
+    cond do
+      String.contains?(url, URI.encode("1 + 1")) -> { :ok, { [], [], @wolfram_xml } }
+      true -> { :ok, { [], [], "<queryresult></queryresult>" } }
+    end
+  end
+end
