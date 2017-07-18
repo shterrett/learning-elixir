59ad79433bc1507f9080b8729efba14fef0b83dc
Stuart Terrett <shterrett@gmail.com>
2017-07-17 20:25:55 -0400

    OTP Guidebook Ch 9

---
+++ elixir-and-otp/chucky/lib/server.ex
@@ -0,0 +1,31 @@
+defmodule Chucky.Server do
+  use GenServer
+
+  # API
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], [name: {:global, __MODULE__}])
+  end
+
+
+  def fact do
+    GenServer.call({:global, __MODULE__}, :fact)
+  end
+
+  # Callbacks
+
+  def init([]) do
+    facts =
+      "facts.txt"
+      |> File.read!
+      |> String.split("\n")
+
+    {:ok, facts}
+  end
+
+  def handle_call(:fact, _from, facts) do
+    random_fact = Enum.random(facts)
+
+    {:reply, random_fact, facts}
+  end
+end
